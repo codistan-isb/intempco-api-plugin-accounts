db69aa6a548da9dc0f5632e72282336d234b3d49
awahab1116 <abdul.wahab@codistan.org>
2023-11-02 15:41:54 +0500

    initial commit

---
+++ src/queries/accounts.js
@@ -0,0 +1,65 @@
+/**
+ * @name accounts
+ * @method
+ * @memberof Accounts/NoMeteorQueries
+ * @summary Returns accounts optionally filtered by group IDs
+ * @param {Object} context - an object containing the per-request state
+ * @param {String} input - input for query
+ * @param {String} [input.groupIds] - Array of group IDs to limit the results
+ * @param {String} [input.notInAnyGroups] - Return accounts that aren't part of any groups
+ * @returns {Promise} Mongo cursor
+ */
+export default async function accounts(context, input) {
+  const { collections } = context;
+  const { Accounts } = collections;
+  const { groupIds, notInAnyGroups, searchQuery } = input;
+
+  await context.validatePermissions("reaction:legacy:accounts", "read");
+
+  const selector = {};
+
+  console.log("searchQuery is ", searchQuery);
+
+  if (searchQuery) {
+    selector.$or = [
+      {
+        "profile.firstName": {
+          $regex: new RegExp(searchQuery, "i"),
+        },
+      },
+      {
+        "profile.lastName": {
+          $regex: new RegExp(searchQuery, "i"),
+        },
+      },
+      {
+        "emails.0.address": {
+          $regex: new RegExp(searchQuery, "i"),
+        },
+      },
+    ];
+  }
+
+  if (groupIds && notInAnyGroups) {
+    selector.$or = [
+      {
+        groups: {
+          $in: groupIds,
+        },
+      },
+      {
+        groups: {
+          $in: [null, []],
+        },
+      },
+    ];
+  } else if (groupIds) {
+    selector.groups = { $in: groupIds };
+  } else if (notInAnyGroups) {
+    selector.groups = { $in: [null, []] };
+  }
+
+  console.log("Selecter is ", selector);
+
+  return Accounts.find(selector);
+}
---
+++ src/resolvers/Account/index.js
@@ -0,0 +1,39 @@
+import _ from "lodash";
+import getCurrencyDefinitionByCode from "@reactioncommerce/api-utils/getCurrencyDefinitionByCode.js";
+import { encodeAccountOpaqueId } from "../../xforms/id.js";
+import addressBook from "./addressBook.js";
+import adminUIShops from "./adminUIShops.js";
+import groups from "./groups.js";
+
+export default {
+  _id: (account) => encodeAccountOpaqueId(account._id),
+  addressBook,
+  adminUIShops,
+  bio: (account) => account.profile.bio,
+  currency: (account) => getCurrencyDefinitionByCode(account.profile && account.profile.currency),
+  emailRecords: (account) => account.emails,
+  firstName: (account) => account.profile.firstName,
+  groups,
+  lastName: (account) => account.profile.lastName,
+  phone: (account) => account.profile.phone,
+  language: (account) => account.profile.language,
+  name: (account) => account.profile.name || account.name,
+  picture: (account) => account.profile.picture,
+  languageAccount: (account) => account.profile.languageAccount,
+  industry: (account) => account.profile.industry,
+  company: (account) => account.profile.company,
+  position: (account) => account.profile.position,
+  addressAccount: (account) => account.profile.addressAccount,
+  cityAccount: (account) => account.profile.cityAccount,
+  stateAccount: (account) => account.profile.stateAccount,
+  countryAccount: (account) => account.profile.countryAccount,
+  zipcode: (account) => account.profile.zipcode,
+  telephone1: (account) => account.profile.telephone1,
+  telephone2: (account) => account.profile.telephone2,
+  preferences: (account) => _.get(account, "profile.preferences"),
+  primaryEmailAddress: (account) => {
+    const primaryRecord = (account.emails || []).find((record) => record.provides === "default");
+    return (primaryRecord && primaryRecord.address) || "";
+  },
+  username: (account) => account.profile.username || account.username,
+};
